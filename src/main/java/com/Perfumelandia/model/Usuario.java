d883c6d75868cc3baeb473f40f8153e24e8c4b50
doraiimond <carl.acevedom@duocuc.cl>
2025-05-25 01:32:53 -0400

    Inicisio de sesion (Completo)

--- src/main/java/com/Perfumelandia/model/Usuario.java
+++ src/main/java/com/Perfumelandia/model/Usuario.java
@@ -1,7 +1,12 @@
 package com.Perfumelandia.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.Optional;
 
 @Entity
