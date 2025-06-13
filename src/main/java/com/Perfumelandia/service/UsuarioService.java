54fa191d7d8f9fc4a2f683ab8a66e6b90fa2f872
The_MisterG <robi.contreras@duocuc.cl>
2025-06-13 17:09:56 -0400

    a
    a

--- src/main/java/com/Perfumelandia/service/UsuarioService.java
+++ src/main/java/com/Perfumelandia/service/UsuarioService.java
@@ -1,45 +1,24 @@
-package com.Perfumelandia.service;
+package com.example.bibliotecaduocV2.Service;
 
-import com.Perfumelandia.model.Usuario;
-import com.Perfumelandia.repository.UsuarioRepository;
+import com.example.bibliotecaduocV2.Model.usuario;
+import com.example.bibliotecaduocV2.Repository.usuarioRepository;
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
-
-
 
 @Service
-public class UsuarioService {
+public class usuarioService {
 
-    @Autowired
-    private UsuarioRepository usuarioRepo;
-
-    public Usuario saveUsuario(Usuario u) {
-        return usuarioRepo.save(u);
+    @Autowired //Inyectar el repositorio de usuario
+    private usuarioRepository repo;
+    //Método para registrar usuarios
+    public usuario registrar(usuario u) {
+        return repo.save(u);//Verificar si el usuario ya existe en la base de dato
     }
-
-    public List<Usuario> getUsuarios() {
-        return usuarioRepo.findAll();
+    //Método que autentifica los usuarios
+    public Optional<usuario> autenticar(String email, String password) {
+        return repo.findByEmail(email).filter(u -> u.getPassword().equals(password));
     }
-
-    public Usuario getUsuarioId(int id) {
-        return usuarioRepo.findById(id).orElse(null);
-    }
-
-    public Usuario updateUsuario(Usuario u) {
-        return usuarioRepo.save(u);
-    }
-
-    public String deleteUsuario(int id) {
-        usuarioRepo.deleteById(id);
-        return "Usuario eliminado";
-    }
-
-    public int totalUsuarios() {
-        return usuarioRepo.findAll().size();
-    }
-
-    public Usuario login(String email, String password) {
-        return usuarioRepo.findByEmailAndPassword(email, password);
-    }    
 }
